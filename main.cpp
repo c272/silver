6be90a0c5a9cb67435d1167d89280614d60c5d5d
C272 <realrussianmanconfirmed@gmail.com>
2018-09-05 14:33:17 +0100

    Basic skeleton for main.

---
+++ main.cpp
@@ -0,0 +1,9 @@
+// MAIN.CPP
+// Here, the engine is initialized, and all other Silver libraries are accessed through header files.
+// Edit this file ONLY unless you want to change the core engine!
+
+int main(int argc, char* argv[]) {
+    //to be expanded
+    
+    //EDIT HERE!
+}
