136e51e680389036f0735918420f32f2ffd997bd
C272 <realrussianmanconfirmed@gmail.com>
2018-09-07 06:58:47 +0100

    Added VS solution; still buildable on MinGW.

---
+++ Silver/Silver/main.cpp
@@ -0,0 +1,12 @@
+//Importing all system dependencies.
+#include <iostream>
+#include <stdio.h>
+#include <SDL.h>
+#include <glew.h>
+#include <string>
+#include <vector>
+
+//Importing all Silver engine headers.
+int main(int argc, char* argv[]) {
+
+}
