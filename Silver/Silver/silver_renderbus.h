f2bb1d5df9f7e7d84a9e702256d30dfdb98733a6
C272 <realrussianmanconfirmed@gmail.com>
2018-09-10 09:12:05 +0100

    Basic render bus outline.

---
+++ Silver/Silver/silver_renderbus.h
@@ -0,0 +1,13 @@
+#ifndef SILVER_RENDERBUS_H
+#define SILVER_RENDERBUS_H
+//Header guarding.
+
+//Including basic Silver headers.
+#include "silver_inc.h"
+
+//Creating GUI render pipeline on the heap. This is globally accessible.
+extern std::vector<Primitive> renderObjects;
+extern std::vector<int> renderLayers;
+
+
+#endif
