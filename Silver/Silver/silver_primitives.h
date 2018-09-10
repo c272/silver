421c742d1c09b20a63bb874ce88d60583facd866
C272 <realrussianmanconfirmed@gmail.com>
2018-09-10 08:46:09 +0100

    Added primitive class outline.

--- Silver/Silver/silver_primitives.h
+++ Silver/Silver/silver_primitives.h
@@ -2,6 +2,30 @@
 #define SILVER_PRIMITIVES_H
 //Header guarding.
 
-//Class for primitives.
+//Including core includes.
+#include "silver_inc.h"
+
+//All functions are wrapped in the SLVR namespace.
+//All documentation for class functions is contained within CPP files, and not in the header.
+
+// SECTION 0
+// Enumerations
+
+namespace slvr {
+
+	// SECTION I
+	// Classes
+
+	//"Primitive" class, for drawing primitives.
+  class Primitive {
+  public:
+    ~Primitive();
+    void render();
+    bool setVertices(float* input);
+  private:
+    float* vertices = nullptr;
+    GLuint vbo;
+    GLuint vao;
+  }
 
 #endif
