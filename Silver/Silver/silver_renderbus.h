b17dbfa04ac290e622a1e3fc35c57632a01a8c59
C272 <realrussianmanconfirmed@gmail.com>
2018-09-10 10:52:12 +0100

    Updated vectors to pointers, changed the name of the pipeline sorting function.

--- Silver/Silver/silver_renderbus.h
+++ Silver/Silver/silver_renderbus.h
@@ -8,10 +8,10 @@
 //Wrapping all in SLVR namespace.
 namespace slvr {
   //Creating GUI render pipeline on the heap. This is globally accessible.
-  extern std::vector<Primitive> renderObjects;
-  extern std::vector<int> renderLayers;
+  extern std::vector<Primitive>* renderObjects;
+  extern std::vector<int>* renderLayers;
 
   //Adding pipeline functions within the namespace.
-  void sortGUIPipeline();
+  void guiSortPipeline();
 }
 #endif
