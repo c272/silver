07cff3fcc7295a03735faf6d15017d32c9561be0
C272 <realrussianmanconfirmed@gmail.com>
2018-09-26 12:44:23 +0100

    Adding texture mapping uniform to the vertex shader.

--- Silver/Silver/vertex.glsl
+++ Silver/Silver/vertex.glsl
@@ -1,6 +1,10 @@
 #version 330 core
 layout (location=0) in vec3 position;
+//Adding uniform for texture mapping.
+uniform vec3 tmap;
+out vec3 textureLocs;
 
 void main() {
+	textureLocs = tmap;
 	gl_Position = vec4(position.xyz, 1.0);
 }
